3a3601e4a1b199051db13bd2a7c1307db383aab9
Carbon <Cheeseburgerbbgun@gmail.com>
2023-03-29 13:47:44 -0600

    Added a general memery version of largest num

---
+++ chap_6/largest_value/largest_value_index.s
@@ -0,0 +1,41 @@
+.globl _start
+
+.section .data
+numofnums:
+  .quad 7
+
+mynumbers: 
+  .quad 5, 20, 33, 80, 52, 10, 1 
+
+.section .text
+_start:
+  movq numofnums, %rcx
+  movq $0, %rdx
+
+#Using rdi to hold high value
+  movq $0, %rdi
+
+#Check for no numbers
+  cmp $0, %rcx
+  je endloop
+
+myloop:
+  movq mynumbers(,%rbx,8), %rax
+
+#Is rdi bigger than rax?
+  cmp %rdi, %rax
+  jbe loopcontrol
+
+  movq %rax, %rdi
+
+loopcontrol:
+  incq %rbx
+
+  loopq myloop
+
+endloop:
+  movq $60, %rax
+  syscall
+
+
+
