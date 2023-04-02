424096347597c3ebec933e747aa721b46438b258
Carbon <Cheeseburgerbbgun@gmail.com>
2023-04-02 10:37:20 -0600

    added uppercasecount, renamed directry to match

---
+++ chap_7/lettercount/uppercasecount.s
@@ -0,0 +1,40 @@
+.globl _start
+.section .data
+mytext:
+  .ascii "This is a string of characters. Here Are Some More UPPERCASE ChaRaCters\0"
+# 17 uppercase characters
+
+.section .text
+  _start:
+    #Initialize
+#Point to string
+  movq $mytext, %rbx
+
+# Set count to 0
+  movq $0, %rdi
+
+mainloop:
+  movb (%rbx), %al
+# if byte is 
+  # Null
+  cmpb $0, %al
+  je finish
+
+# bigger than 'A'
+  cmpb $'A', %al
+  jb loopcontrol
+
+# Less than 'Z'
+  cmpb $'Z', %al
+  ja loopcontrol
+
+# uppercase!
+  incq %rdi
+
+loopcontrol:
+  incq %rbx
+  jmp mainloop
+
+finish:
+  movq $60, %rax
+  syscall
