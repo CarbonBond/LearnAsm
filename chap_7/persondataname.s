68882546c5a9bfe231fcb6ea78d2e4ca11b4e438
Carbon <Cheeseburgerbbgun@gmail.com>
2023-04-03 12:26:08 -0600

    Added a name ptr and some misc data for exercise

---
+++ chap_7/persondataname.s
@@ -0,0 +1,44 @@
+.section .data
+
+.globl people, numpeople
+numpeople:
+  .quad (endpeople - people)/PERSON_RECORD_SIZE
+
+jcname:
+  .ascii "Jon Conrad\0"
+hdname:
+  .ascii "Hera Dono\0"
+kdname:
+  .ascii "Ken Dono\0"
+rcname:
+  .ascii "Ren Chester\0"
+inname:
+  .ascii "Isaac Newton\0"
+clname:
+  .ascii "Clint Lewis\0"
+
+people:
+  .quad jcname, 101, 11, 3, 3, 1, 1, 54, 44
+  .quad hdname, 120, 11, 1, 1, 2, 1, 61, 32
+  .quad kdname, 140, 12, 4, 2, 3, 0, 76, 18
+  .quad rcname, 160,  8, 3, 4, 4, 0, 68, 32
+  .quad inname, 180,  8, 1, 1, 2, 1, 81, 23
+  .quad clname, 200, 10, 5, 5, 3, 0, 76, 20
+endpeople:
+
+
+.globl NAME_OFFSET, WEIGHT_OFFSET, SHOE_OFFSET
+.globl EYEC_OFFSET, SKINC_OFFSET, SEX_OFFSET
+.globl HAIR_OFFSET, HEIGHT_OFFSET, AGE_OFFSET
+.equ NAME_OFFSET, 0
+.equ WEIGHT_OFFSET, 8 
+.equ SHOE_OFFSET, 16
+.equ EYEC_OFFSET, 24
+.equ SKINC_OFFSET, 32
+.equ SEX_OFFSET, 40
+.equ HAIR_OFFSET,48 
+.equ HEIGHT_OFFSET, 56 
+.equ AGE_OFFSET, 64 
+
+.globl PERSON_RECORD_SIZE
+.equ PERSON_RECORD_SIZE, 72 
