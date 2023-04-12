7c1504239a80bb17b147c46aa9c83be8775f00d8
Carbon <Cheeseburgerbbgun@gmail.com>
2023-04-12 11:10:36 -0600

    Added a local variable const to make programe more readable

--- chap_11/recursiveFactorial/factorial.s
+++ chap_11/recursiveFactorial/factorial.s
@@ -1,13 +1,17 @@
 .globl factorial
 .type factorial, @function
 
+
+#Local stack constant
+.equ LOCAL_NUM, -8
+
 .section .text
 factorial:
 #rdi: current number
 
 #Store one local vaiable -8(%rbp)
   enter $16, $0
-  movq %rdi, -8(%rbp)
+  movq %rdi, LOCAL_NUM(%rbp)
   movq $1, %rax
 
 continue:
@@ -18,7 +22,7 @@ continue:
   call factorial
 
 multiply:
-  mulq -8(%rbp)
+  mulq LOCAL_NUM(%rbp)
   leave 
   ret
   
