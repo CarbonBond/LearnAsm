b9a4146f022e425d6d81cb9122ebef873508f7f3
Carbon <Cheeseburgerbbgun@gmail.com>
2023-04-19 11:38:54 -0600

    Scanf prog that counts bits

---
+++ chap_12/countbitsscanf/countbitscanf.s
@@ -0,0 +1,42 @@
+.globl main
+.section .data
+promptformat:
+  .ascii "Enter a number, then press return.\n\0"
+
+scanformat:
+  .ascii "%d\0"
+
+resultformat:
+  .ascii "The result is %d. \n\0"
+
+.section .text
+.equ LOCAL_NUMBER, -8
+main:
+  #Two local values
+  enter $16, $0
+
+#Prompt
+  movq stdout, %rdi
+  movq $promptformat, %rsi
+  movq $0, %rax
+  call fprintf
+
+#Request input from user
+  movq stdin, %rdi
+  movq $scanformat, %rsi
+  leaq LOCAL_NUMBER(%rbp), %rdx
+  movq $0, %rax
+  call fscanf
+
+  movq LOCAL_NUMBER(%rbp), %rdi
+  call count_bits 
+
+  movq stdout, %rdi
+  movq $resultformat, %rsi
+  movq %rax, %rdx
+  movq $0, %rax
+  call fprintf
+  
+  leave
+  ret
+
