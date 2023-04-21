7e28746933bae96ec02608c0e7f4a359d3c5ee6f
Carbon <Cheeseburgerbbgun@gmail.com>
2023-04-21 10:15:02 -0600

    Change to use a second local for exiting.

--- chap_12/countbitloop/countbitloop.s
+++ chap_12/countbitloop/countbitloop.s
@@ -1,10 +1,14 @@
 .globl main
 .section .data
 promptformat:
-  .ascii "Enter a number followed by a 1 to exit, 0 to continue, then press return.\n\0"
+  .ascii "Enter a number, then press return.\n\0"
+exitformat:
+  .ascii "Do you want to exit? [y]/n\n\0"
 
 scanformat:
-  .ascii "%d %d\0"
+  .ascii "%d\0"
+charformat:
+  .ascii "%c\0"
 
 resultformat:
   .ascii "The result is %d. \n\0"
@@ -18,6 +22,8 @@ main:
   enter $16, $0
 
 mainloop:
+#Move default exit into EXIT EXIT_NUMBER
+  movq $'y', EXIT_NUMBER(%rbp)
 #Prompt
   movq stdout, %rdi
   movq $promptformat, %rsi
@@ -28,7 +34,6 @@ mainloop:
   movq stdin, %rdi
   movq $scanformat, %rsi
   leaq LOCAL_NUMBER(%rbp), %rdx
-  leaq EXIT_NUMBER(%rbp), %rcx
   movq $0, %rax
   call fscanf
 
@@ -40,9 +45,22 @@ mainloop:
   movq %rax, %rdx
   movq $0, %rax
   call fprintf
+
+#Prompt Exit
+  movq stdout, %rdi
+  movq $exitformat, %rsi
+  movq $0, %rax
+  call fprintf
+
+#Request input from user
+  movq stdin, %rdi
+  movq $charformat, %rsi
+  leaq EXIT_NUMBER(%rbp), %rdx
+  movq $0, %rax
+  call fscanf
   
-  cmpq $0, EXIT_NUMBER(%rbp)
-  je mainloop
+  cmpq $'y', EXIT_NUMBER(%rbp)
+  jne mainloop
 
   leave
 
